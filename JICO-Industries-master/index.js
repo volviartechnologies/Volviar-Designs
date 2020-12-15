72693c5c20557eadc9f15aa966b022e78c366719
Afsal Malabary <afsalmalabary@gmail.com>
2020-12-16 00:31:30 +0530

    latest file of monthly salary process

---
+++ JICO-Industries-master/index.js
@@ -0,0 +1,13 @@
+﻿function sendEmail() {
+    Email.send({
+        Host: "mail.jicosolar.com",
+        Username: "<noreply@jicosolar.com>",
+        Password: "<2e12Axz_>",
+        To: '<info@jicosolar.com>',
+        From : "<divyapriyaub12@gmail.com>",
+        Subject : "<hai>",
+        Body : "<s>",
+    }).then(
+		message = alert("mail sent successfully")
+	);
+}
